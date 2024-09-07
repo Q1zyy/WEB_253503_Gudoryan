c299f6661bd70aeca4994bdfb9f5e5fc8f5668aa
q1zyy <gudoryanjackson@mail.ru>
2024-09-07 21:47:27 +0300

    start adding db

---
+++ WEB_253503_Gudoryan.API/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEB_253503_Gudoryan.Domain.Entities;
+
+namespace WEB_253503_Gudoryan.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+
+    }
+}
---
+++ WEB_253503_Gudoryan.API/Data/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿namespace WEB_253503_Gudoryan.API.Data
+{
+    public class DbInitializer
+    {
+
+        public static async Task SeedData(WebApplication app)
+        {
+
+        }
+
+    }
+}
