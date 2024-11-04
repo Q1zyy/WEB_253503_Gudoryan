77bb4cf5eaef8fd761f57314933c4486e3dc6b3d
q1zyy <gudoryanjackson@mail.ru>
2024-11-04 13:47:57 +0300

    lab11 done

---
+++ WEB_253503_Gudoryan.SSR/Models/InputNumberModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEB_253503_Gudoryan.SSR.Models
+{
+    public class InputNumberModel
+    {
+
+        [Range(1, 10, ErrorMessage = "Value must be between 1 and 10")]
+        public int Value { get; set; }
+
+    }
+}
